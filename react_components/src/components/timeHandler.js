e0917253ea24367c6dd3511614e09c7b038d41de
Jkcadee <jkwok55@my.bcit.ca>
2020-10-17 19:30:36 -0700

    state

--- react_components/src/components/timeHandler.js
+++ react_components/src/components/timeHandler.js
@@ -42,14 +42,14 @@ class TimeHandler extends React.Component {
   }
 
   tickDownTimer() {
-    console.log("cdmount is being run");
+   // console.log("cdmount is being run");
     if (!this.state.timerIsPaused) {
-      console.log("in here");
-      let formerState = this.state.currentTimeInSeconds--;
-      this.setState((state) => ({
+      //console.log("in here");
+      let formerState = this.state.currentTimeInSeconds - 1;
+      this.setState({
         // reduce number of seconds.. every second
         currentTimeInSeconds: formerState,
-      }));
+      });
       console.log(this.state);
       if (this.state.currentTimeInSeconds === 0) {
         this.resetTimer();
